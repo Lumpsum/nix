852f0498a6b08980f27f3a78f668e71f5ff72fc2
Lumpsum <vergunstje@hotmail.com>
2025-04-18 00:30:42 +0200

    Format flakenix

--- flake.nix
+++ flake.nix
@@ -5,16 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     home-manager = {
-	    url = "github:nix-community/home-manager";
-	    inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     zen-browser = {
-        url = "github:0xc000022070/zen-browser-flake";  
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # wezterm.url = "github:wez/wezterm?dir=nix";
@@ -24,73 +24,78 @@
     nvim-nix.url = "github:Lumpsum/nvim-nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin, nvim-nix, ... } @ inputs:
+  outputs =
     {
-        nixosConfigurations = {
-            lumpsum = let
-                username = "lumpsum";
-            in
-            nixpkgs.lib.nixosSystem {
-                modules = [
-                    ./hosts/home/configuration.nix
-                    inputs.stylix.nixosModules.stylix
-                
-                    home-manager.nixosModules.home-manager
-                    {
-                        home-manager.extraSpecialArgs = { 
-                            inherit inputs; 
-			                inherit nvim-nix;
-                            theme = "kanagawa";
-                        };
-                        home-manager.users.lumpsum = import users/${username}/home.nix;
-                    }
-                ];
-            };
-        };
+      self,
+      nixpkgs,
+      home-manager,
+      darwin,
+      nvim-nix,
+      ...
+    }@inputs:
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
 
+      nixosConfigurations = {
+        lumpsum =
+          let
+            username = "lumpsum";
+          in
+          nixpkgs.lib.nixosSystem {
+            modules = [
+              ./hosts/home/configuration.nix
+              inputs.stylix.nixosModules.stylix
 
-        darwinConfigurations = {
-            rickvergunst = let
-                username = "rickvergunst";
-            in
-                darwin.lib.darwinSystem rec {
-                    system = "aarch64-darwin";
-                    modules = [
-                        ./hosts/mac/configuration.nix
-                        home-manager.darwinModules.home-manager
-                        {
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            home-manager.backupFileExtension = "backup";
-                            home-manager.extraSpecialArgs = { 
-                                inherit inputs;
-                                inherit nvim-nix;
-                                theme = "ashen";
-                                # nvim-nix = nvim-nix.packages.aarch64-darwin.default;
-                            };
-                            home-manager.users.rickvergunst = import users/${username}/home.nix;
-                        }
-                    ];
-              };
-        };
-	    homeConfigurations = {
-      		# FIXME replace with your username@hostname
-      		"lumpsum@Lumpsum" = home-manager.lib.homeManagerConfiguration {
-                pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-                extraSpecialArgs = {
-                    inherit inputs;
-			        inherit nvim-nix;
-                    theme = "ashen"; 
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  inherit nvim-nix;
+                  theme = "kanagawa";
                 };
-                # > Our main home-manager configuration file <
-                modules = [
-                    users/arch/home.nix
-                    # {
-                    #     home.packages = [ nvim-nix.packages.x86_64-linux.default ];
-                    # }
-                ];
-            };
+                home-manager.users.lumpsum = import users/${username}/home.nix;
+              }
+            ];
+          };
+      };
+
+      darwinConfigurations = {
+        rickvergunst =
+          let
+            username = "rickvergunst";
+          in
+          darwin.lib.darwinSystem rec {
+            system = "aarch64-darwin";
+            modules = [
+              ./hosts/mac/configuration.nix
+              home-manager.darwinModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = "backup";
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  inherit nvim-nix;
+                  theme = "ashen";
+                };
+                home-manager.users.rickvergunst = import users/${username}/home.nix;
+              }
+            ];
+          };
+      };
+      homeConfigurations = {
+        "lumpsum@Lumpsum" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = {
+            inherit inputs;
+            inherit nvim-nix;
+            theme = "ashen";
+          };
+          modules = [
+            users/arch/home.nix
+          ];
         };
+      };
 
     };
 }
